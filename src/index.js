b3d9c28f53feea82abf0c5dcac86b720d329cae9
Sthephany Granados <lsgranados04@gmail.com>
2020-06-05 15:45:45 -0500

    Se realiza el codigo de validacion de tarjeta

--- src/index.js
+++ src/index.js
@@ -1,27 +1,81 @@
-import validator from './validator.js';
+//import validator from './validator.js';
 
-console.log(validator);
-
-const numeroTarjeta = document.getElementById ('entradaNumero');
-
+//console.log(validator);
+let numeroTarjeta = document.getElementById("entradaNumero");
+let botonValidar = document.getElementById("btnValidar");
+let mensajeValidacion = document.getElementById("mensaje");
+let botonComprar = document.getElementById("mensajeBoton");
+botonValidar.addEventListener("click", validar);
 let inputNumero;
+let arreglo = [];
 
-numeroTarjeta.addEventListener ('keyup', (e) => {
-  inputNumero = e.target.value;  
+numeroTarjeta.addEventListener("keyup", recibirNumero);
+
+function recibirNumero(evento) {
+  mensajeValidacion.innerHTML = "";
+  inputNumero = evento.target.value;
 
   numeroTarjeta.value = inputNumero
 
-  .replace(/\s/g, '')  // elimina los espacios
-  .replace(/\D/g, '')  // elimina las letras
-  .replace(/([0-9]{4})/g, '$1 ');  //agrupa los numero de a 4
+    .replace(/\s/g, "") // elimina los espacios
+    .replace(/\D/g, "") // elimina las letras
+    .replace(/([0-9]{4})/g, "$1 "); //agrupa los numero de a 4
 
-  console.log(numeroTarjeta.value)
- }
-)
+  console.log(inputNumero);
+
+  if (inputNumero.length > 18) {
+    botonValidar.style.display = "block";
+  }
+}
+
+function validar() {
+  pasarArreglo(inputNumero);
+  console.log(arreglo);
+  luhn();
+}
+
+function pasarArreglo(numero) {
+  let arrayNumeros = [];
+  for (let i = 0; i < numero.toString().length; i++) {
+    arrayNumeros.push(numero.toString()[i]);
+  }
+
+  console.log(arrayNumeros);
+  arrayNumeros = arrayNumeros.filter((i) => i !== " "); // quitamos los espacios
+  console.log(arrayNumeros.length);
+
+  for (let i = 0; i < arrayNumeros.length; i++) {
+    arreglo.push(parseInt(arrayNumeros[i]));
+    console.log(arreglo.length);
+  }
+  arreglo.reverse();
+}
+
+function luhn() {
+  let suma = 0;
+  for (let i = 1; i < 16; i += 2) {
+    arreglo[i] = arreglo[i] * 2;
+
+    if (arreglo[i] > 9) { 
+      console.log(arreglo[i]);
+      console.log(arreglo[i].toString()[0]);
+      console.log(arreglo[i].toString()[1]);
+      arreglo[i] = parseInt(arreglo[i].toString()[0]) + parseInt(arreglo[i].toString()[1])
+    }
+  }
+  for (let i = 0; i < 16; i++) {
+    suma += arreglo[i];    
+  }
+  console.log(suma);
   
- /* arregloNumero.forEach(function(element,index,arreglo){
-    let number = parseInt(element);
-    console.log(number);    
-  })  */
+  if ((suma % 10) == 0) {
+    mensajeValidacion.innerHTML = "Tu tarjeta es valida para comprar"; 
+    botonComprar.style.display = "block";
 
+  } else {
+    mensajeValidacion.innerHTML = "Tu tarjeta NO es valida"; 
+    numeroTarjeta.value = "";
+    botonValidar.style.display = "none";
+  }
+}
 
