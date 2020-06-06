008a7079de9601b32a794a5f6b6c71c23fd077b3
Sthephany Granados <lsgranados04@gmail.com>
2020-06-05 20:52:57 -0500

    Se realizan la integracion del codigo utilizando modulos

--- src/index.js
+++ src/index.js
@@ -1,119 +1,64 @@
+import validator from './validator.js';
 
-//console.log(validator);
+//console.log(validator.isValid);
 
+//Definicion de variables
 let numeroTarjeta = document.getElementById("entradaNumero");
 let botonValidar = document.getElementById("btnValidar");
 let mensajeValidacion = document.getElementById("mensaje");
 let botonComprar = document.getElementById("mensajeBoton");
-botonValidar.addEventListener("click", validar);
+let botonNext = document.getElementById("botonNext")
 let inputNumero;
-let arreglo = [];
+let pantalla = document.getElementById("pantalla2");
+let pantalla1 = document.getElementById("pantalla1");
 
+//Declaracion de eventos
+botonValidar.addEventListener('click', validar);
+botonNext.addEventListener('click', pulsarBoton);
 
 numeroTarjeta.addEventListener("keyup", recibirNumero);
 
 function recibirNumero(evento) {
   mensajeValidacion.innerHTML = "";
   inputNumero = evento.target.value;
-  let enmascarado = numeroTarjeta.value
+  let enmascarado = numeroTarjeta.value;
 
-  enmascarado = enmascarar(numeroTarjeta.value)
-  
-  document.getElementById("contenedor").innerHTML = enmascarado.replace(/([*,0-9]{4})/g, "$1 ");
+  enmascarado = validator.maskify(inputNumero);
+
+  document.getElementById("contenedor").innerHTML = enmascarado.replace(
+    /([*,0-9]{4})/g,
+    "$1 "
+  );
 
   numeroTarjeta.value = inputNumero
 
-
     .replace(/\s/g, "") // elimina los espacios
-    .replace(/\D/g, "") // elimina las letras
-    // .replace(/([0-9]{4})/g, "$1 "); //agrupa los numero de a 4
+    .replace(/\D/g, ""); // elimina las letras
+  
+  //console.log(inputNumero);
 
-  console.log(inputNumero);
-
-  if (inputNumero.length > 18) {
+  if (inputNumero.length > 15) {
     botonValidar.style.display = "block";
   }
 }
 
-function enmascarar(Real){
-    let Mostrar = ""
-    console.log(Real)
-    for (let index = 0; index < Real.length; index++) {
-        if(index < Real.length - 4)
-        {
-            Mostrar += "*"
-        }
-        else
-        {
-            Mostrar=Mostrar + Real[index]
-        }
-    }
-    
-    return Mostrar
-}
-
-function validar() {
-  pasarArreglo(inputNumero);
-  console.log(arreglo);
-  luhn();
-}
-
-function pasarArreglo(numero) {
-  let arrayNumeros = [];
-  for (let i = 0; i < numero.toString().length; i++) {
-    arrayNumeros.push(numero.toString()[i]);
-  }
-
-  console.log(arrayNumeros);
-  arrayNumeros = arrayNumeros.filter((i) => i !== " "); // quitamos los espacios
-  console.log(arrayNumeros.length);
-
-  for (let i = 0; i < arrayNumeros.length; i++) {
-    arreglo.push(parseInt(arrayNumeros[i]));
-    console.log(arreglo.length);
-  }
-  arreglo.reverse();
-}
-
-function luhn() {
-  let suma = 0;
-  for (let i = 1; i < 16; i += 2) {
-    arreglo[i] = arreglo[i] * 2;
-
-    if (arreglo[i] > 9) { 
-      console.log(arreglo[i]);
-      console.log(arreglo[i].toString()[0]);
-      console.log(arreglo[i].toString()[1]);
-      arreglo[i] = parseInt(arreglo[i].toString()[0]) + parseInt(arreglo[i].toString()[1])
-    }
-  }
-  for (let i = 0; i < 16; i++) {
-    suma += arreglo[i];    
-  }
-  console.log(suma);
-  
-  if ((suma % 10) == 0) {
-    mensajeValidacion.innerHTML = "Tu tarjeta es valida para comprar"; 
+function validar() {  
+  if (validator.isValid(inputNumero) == true) {
+    mensajeValidacion.innerHTML = "Tu tarjeta es valida para comprar";
     botonComprar.style.display = "block";
-
   } else {
-    mensajeValidacion.innerHTML = "Tu tarjeta NO es valida"; 
+    mensajeValidacion.innerHTML = "Tu tarjeta NO es valida";
     numeroTarjeta.value = "";
     botonValidar.style.display = "none";
   }
 }
 
-function ingresarNombre(){
-
-    var nombre=document.getElementById("entradaNombre").value
-    document.getElementById("name").innerHTML= nombre.toUpperCase() 
-
+function ingresarNombre() {
+  let nombre = document.getElementById("entradaNombre").value;
+  document.getElementById("name").innerHTML = nombre.toUpperCase();
 }
- function pulsarboton(){
 
-    var pantalla= document.getElementById("pantalla2")
-    pantalla.style.display="block"
-
-    var pantalla1=document.getElementById("pantalla1")
-    pantalla1.style.display="none"
- }
+function pulsarBoton() {  
+  pantalla.style.display = "block";  
+  pantalla1.style.display = "none";
+}
--- src/validator.js
+++ src/validator.js
@@ -1,10 +1,55 @@
 const validator = {
+
+    isValid: function (numero) {
+        let arrayNumeros = [];
+        let arreglo = [];
+        let suma = 0;
+
+        for (let i = 0; i < numero.toString().length; i++) {
+            arrayNumeros.push(numero.toString()[i]);
+        }
+        //console.log(arrayNumeros);
+        
+        for (let i = 0; i < arrayNumeros.length; i++) {
+            arreglo.push(parseInt(arrayNumeros[i]));
+            //console.log(arreglo.length);
+        }
+        arreglo.reverse();
+
+        for (let i = 1; i < 16; i += 2) {
+            arreglo[i] = arreglo[i] * 2;
+
+                if (arreglo[i] > 9) {
+                    arreglo[i] =
+                    parseInt(arreglo[i].toString()[0]) + parseInt(arreglo[i].toString()[1]);
+                }
+        }
+
+        for (let i = 0; i < 16; i++) {
+            suma += arreglo[i];
+        }
+        //console.log(suma);
+
+        if (suma % 10 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    },
  
+    maskify: function (Real) {
+        let Mostrar = "";
+        //console.log(Real);
+        for (let index = 0; index < Real.length; index++) {
+          if (index < Real.length - 4) {
+            Mostrar += "*";
+          } else {
+            Mostrar = Mostrar + Real[index];
+          }
+        }      
+        return Mostrar;
+    }
 };
 
-function enmascarar(valor){
-    let enmasc = valor.replace(/\d{4}(?= \d{4})/g, "****");
-    return enmasc
-}
 
 export default validator;
